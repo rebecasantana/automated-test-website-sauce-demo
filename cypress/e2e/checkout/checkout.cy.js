c668eba269cb15e2f00e3530d7f2a33ceb0d93d6
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-09-22 23:44:43 -0300

    Atualizando o projeto - flow de chekout

--- cypress/e2e/checkout/checkout.cy.js
+++ cypress/e2e/checkout/checkout.cy.js
@@ -1,38 +1,71 @@
 /// <reference types="cypress" />
 
 // Implementação dos casos de teste da página de checkout.
-describe("Cenário de Checkout", () => {
-  it("Deve realizar Checkout com sucesso", () => {
-    cy.visit("https://www.saucedemo.com/");
-    cy.get('[data-test="username"]').type("standard_user");
-    cy.get('[data-test="password"]').type("secret_sauce");
-    cy.get('[data-test="login-button"]').click();
-    cy.get('[class="shopping_cart_link"]').click();
-    cy.get('button[id="checkout"]').click();
+
+beforeEach(() => {
+  cy.visit('https://www.saucedemo.com/');
+
+  cy.get('#user-name').type('standard_user');
+  cy.get('#password').type('secret_sauce');
+  cy.get('#login-button').click();
+
+  cy.get('.shopping_cart_link').click();
+  cy.get('#checkout').click();
+});
+
+after(() => {
+  cy.get('#finish').click();
+  cy.get('.complete-header').contains("Thank you for your order!");
+})
+
+describe('Cenário de Checkout', () => {
+
+  it('Validar se o firsName não foi preenchido', () => {
+    cy.get('#last-name').type("Santos");
+    cy.get('#postal-code').type("13252-110");
+    cy.get('#continue').click();
+    cy.get('.error-message-container.error').contains("Error: First Name is required");
   });
-});
 
-it('Validar informações da entrega com campo "Zip/Postal Code" apenas com um espaço', () => {
-  cy.get('[data-test="firstName"]').type("Maria");
-  cy.get('[data-test="lastName"]').type("Santos");
-  cy.get('[data-test="postalCode"]').type(" ");
-  cy.get('[data-test="continue"]').click();
-});
+  it('Validar se o LastName não foi preenchido', () => {
+    cy.get('#first-name').type("Maria");
+    cy.get('#postal-code').type("13252-110");
+    cy.get('#continue').click();
+    cy.get('.error-message-container.error').contains("Error: Last Name is required");
+  });
 
-it("Validar informações da entrega com campo “Zip/Postal Code” apenas com 4 caracteres", () => {
-  cy.get('[data-test="firstName"]').type("Maria");
-  cy.get('[data-test="lastName"]').type("Santos");
-  cy.get('[data-test="postalCode"]').type("1111");
-});
+  it('Validar se o Postal Code não foi preenchido', () => {
+    cy.get('#first-name').type("Maria");
+    cy.get('#last-name').type("Santos");
+    cy.get('#continue').click();
+    cy.get('.error-message-container.error').contains("Error: Postal Code is required");
+  });
 
-it("Validar informações da entrega com campo “First Name” com apenas 1 caractere. ", () => {
-  cy.get('[data-test="firstName"]').type("");
-  cy.get('[data-test="lastName"]').type("Santos");
-  cy.get('[data-test="postalCode"]').type("13252-110");
-});
+  it('Verificar informações da entrega passam com campo "Zip/Postal Code" tendo apenas com um espaço', () => {
+    cy.get('#first-name').type("Maria");
+    cy.get('#last-name').type("Santos");
+    cy.get('#postal-code').type(" ");
+    cy.get('#continue').click();
+  });
 
-it("Validar informações da entrega com campo “First Name” com apenas 1 caractere. ", () => {
-  cy.get('[data-test="firstName"]').type("M");
-  cy.get('[data-test="lastName"]').type("Santos");
-  cy.get('[data-test="postalCode"]').type("13252-110");
+  it("Verificar informações da entrega passam com campo “Zip/Postal Code” tendo apenas com 4 caracteres", () => {
+    cy.get('#first-name').type("Maria");
+    cy.get('#last-name').type("Santos");
+    cy.get('#postal-code').type("1111");
+    cy.get('#continue').click();
+  });
+
+  it("Verificar informações da entrega passam com campo “First Name” com apenas 1 caractere. ", () => {
+    cy.get('#first-name').type("M");
+    cy.get('#last-name').type("Santos");
+    cy.get('#postal-code').type("13252-110");
+    cy.get('#continue').click();
+  });
+
+  it("Verificar informações da entrega passam com campo “Last Name” com apenas 1 caractere. ", () => {
+    cy.get('#first-name').type("Maria");
+    cy.get('#last-name').type("S");
+    cy.get('#postal-code').type("13252-110");
+    cy.get('#continue').click();
+  });
 });
